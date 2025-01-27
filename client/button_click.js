5fe0d7275ccbaa0c813a2fdb85b35207e2dfc1b8
Akshaj Pawar <ngcn29@durham.ac.uk>
2025-01-27 14:39:46 +0000

    it actually does things now

--- client/button_click.js
+++ client/button_click.js
@@ -1,39 +1,39 @@
 questions_answered = 0
-cri_ans = 'ABBAC'
-fuc_ans = 'BCACA'
-ver_ans = 'CACBB'
 num_qs = 10
 X = [0, 0, 0]
 ABorC = 'A'
-let warmth = 0
-let grace = 0
-let virtosity = 0
-let attribute_scores = [warmth, grace, virtosity]
-let attribute_keys = ['Warmth', 'Grace', 'Virtosity']
+let attribute_scores = [0, 0, 0]
+let attribute_keys = ['Warmth', 'Grace', 'Virtuosity']
+let flags = 
+{
+    'Lover': 0,
+    'Explorer': 0,
+    'Survivor': 0,
+    'Feral': 0,
+    'Hero': 0,
+    'Dreamer': 0,
+    'Healer': 0,
+    'Orderly': 0,
+    'Wistful': 0,
+    'Utilitarian': 0,
+    'At Peace': 0
+    //currently this feature is not in use
+}
 
 function log_answer()
 {
     questions_answered++
     if (questions_answered <= num_qs){
-        //if (ans == cri_ans[questions_answered-2]){
-        //    crimson++
-        //}
-        //else if (ans == fuc_ans[questions_answered-2]){
-        //    fuchsia++
-        //}
-        //else if (ans == ver_ans[questions_answered-2]){
-        //    verdant++
-        //}
-
         //updating scores
-        console.log(ans)
         for (let i = 0; i < 3; i++){
-            score = attribute_scores[i]
-            key = attribute_keys[i]
-            score += 1
-            //update_score((questions_answered - 2), score, key, ans)
+            key = attribute_keys[i];
+            extract_score((questions_answered - 2), key, ans)
+            .then(x_score => {
+                attribute_scores[i] += x_score;
+                console.log(attribute_scores[i])
+            })
         }
-        console.log(attribute_scores)
+        console.log(attribute_scores);
 
         //update title
         qstring = questions_answered.toString()
@@ -52,16 +52,17 @@ function log_answer()
     }
     
     if (questions_answered > num_qs) {
-        //if (crimson > 2){
-        //    localStorage.setItem('colour', 'Crimson.html');
-        //}
-        //if (fuchsia > 2){
-        //    fetch_answer('Fuchsia.html')
-        //}
-        //if (verdant > 2){
-        //    fetch_answer('Verdant.html')
-        //}
-        fetch('http://127.0.0.1:8080/final')
+        
+        //normalise attribute_scores
+        //attribute_scores[0] -= 10
+        //attribute_scores[2] -= 10
+        console.log(attribute_scores)
+
+        colour_list = create_colour_list(attribute_scores[0], attribute_scores[1], attribute_scores[2])
+        console.log(colour_list)
+        fit_colour_key = find_fittest_colour(colour_list)
+
+        fetch('http://127.0.0.1:8080/final?colour=' + fit_colour_key)
          .then(response => {
             if (response.ok) {
                 return response.text()
@@ -99,7 +100,6 @@ function update_button(option, index)
     fetch(url)
          .then(response => {
             if (response.ok) {
-                console.log("file exists probably")
                 return response.text()
             }
             else {
@@ -111,29 +111,72 @@ function update_button(option, index)
          })
 }
 
-function update_score(index, score, key, ans)
+function extract_score(index, key, ans)
 {
     const url = 'http://127.0.0.1:8080/api/data/q_data/score?index=' + String(index) + '&attributekey=' + key + '&ans=' + ans;
-    fetch(url)
-    .then(response => {
-       if (response.ok) {
-           console.log("file exists probably")
-           return response.text()
-       }
-       else {
-           console.log('Error: File does not exist')
-       }})
-    .then(body => {
-        console.log(body);
-        int_body = parseInt(body, 10);
-        if (isNaN(int_body)) {
-            console.log('Error: Response is not a valid number');
-        } 
-        else {
-            score += int_body
+    
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(response => {
+           if (response.ok) {
+               return response.text()
+           }
+           else {
+               console.log('Error: File does not exist')
+               resolve(0)
+           }})
+        .then(body => {
+            console.log(body);
+            x_score = parseInt(body, 10);
+            if (isNaN(x_score)) {
+                console.log('Error: Response is not a valid number');
+                resolve(0)
+            } 
+            else {
+                resolve(x_score)
+            }
+        })
+        .catch((error) => {
+           console.log('Error: Could not connect to server')
+           resolve(0)
+        })
+    })
+}
+
+function create_colour_list(W, G, V, flags)
+{
+    //note that the flag feature has no been implemented, if it were to be implemented things wou;ld be reshuffled tor duce the bias
+    colour_list = {
+        'Crimson': ((3*Math.sqrt(2) / 2)*W - (3*Math.sqrt(2) / 4)*G - (3*Math.sqrt(2) / 4)*V), //works
+        'Scarlet': (Math.sqrt(5)*W), //too close to crimson for comfort, also the friendship + passion route is the only way to get to scarlet which isn't exactly what we want
+        'Mahogany': (W - 2*V),
+        'Afterglow': (-(1/2)*W + 2*G + (1/2)*V), //win
+        'Fuchsia': ((3*Math.sqrt(2) / 4)*W + (3*Math.sqrt(2) / 4)*G + (3*Math.sqrt(2) / 2)*V), 
+        'Tyrian': (2*V - G), 
+        'Golden': (2*G + W), //way too clsoe to afterglow for comfort
+        'Mint': ((3*Math.sqrt(2) / 4)*G - (3*Math.sqrt(2) / 4)*W - (3*Math.sqrt(2) / 2)*V), //didnt work, superseded by afterglow, too close to azure
+        'Verdant': ((-2)*V - (1/2)*W - (1/2)*G), //overwhelmed by black, needs flags
+        'Azure': (G - 2*W), 
+        'Cerulean': ((-2)*W - G),
+        'Black': ((-1)*Math.sqrt(5)*G) 
+        //i feel like if we shift grace to more mean order and structure and elegance it solves a lot of these problems
+    }
+    return(colour_list)
+}
+
+function find_fittest_colour(colour_list)
+{
+    let colour_key_list = Object.keys(colour_list);
+    let colour_funct_list = Object.values(colour_list);
+    let max_value = -100;
+    let max_index = 0;
+    for (let i = 0; i < colour_funct_list.length; i++){
+        c_value = colour_funct_list[i];
+        if (c_value > max_value){
+            max_value = c_value;
+            max_index = i;
         }
-    })
-    .catch((error) => {
-       console.log('Error: Could not connect to server')
-    })
+    }
+    console.log(colour_key_list[max_index])
+    return(colour_key_list[max_index])
 }
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static('client'));
 //3 kinds of get methods over here:
 
 let q_data = require("./data/q_data.json")
+let a_data = require("./data/a_data.json")
 
 app.get("/api/data/q_data/questiontext", function(req, resp){
     //get method for question text, takes questions_answered as a parameter
@@ -23,16 +24,16 @@ app.get("/api/data/q_data/optiontext", function(req, resp){
 })
 
 app.get("/api/data/q_data/score", function(req, resp){
-    let opt = req.query.ans
-    let key = req.query.attributekey
+    //gets the score adjustments out of the database
+    let opt = req.query.ans;
+    let key = req.query.attributekey;
     const index = parseInt(req.query.index, 10);
-    resp.send(q_data[0][opt][key][index])
+    resp.send(String(q_data[0][opt][key][index]))
 })
 
-let colour = 'Crimson'
-
 app.get("/final", function(req, resp){
-    resp.send(colour)
+    let colour = req.query.colour
+    resp.send(a_data["Descriptions"][colour])
 })
 
 app.listen(8080, () => console.log('Server running at http://127.0.0.1:8080/'));
